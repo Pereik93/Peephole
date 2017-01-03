9aff6b9ad218f4ebd2c1ec8378a4f15d7e1fb3a1
Per-Olav S. Eikrem <perolaveikrem@hotmail.com>
2017-01-03 14:55:20 +0100

    Added Project
    Added project to public source control

---
+++ UsersTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [UserType]  INT           NOT NULL,
+    [Firstname] NVARCHAR (50) NOT NULL,
+    [Lastname]  NVARCHAR (50) NOT NULL,
+    [Password]  NVARCHAR (32) NOT NULL,
+    [Email]     NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Users_ToTable] FOREIGN KEY ([UserType]) REFERENCES [dbo].[UserType] ([Id])
+);
+
